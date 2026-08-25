24b64dd87ee4f22e8ab751b7a6fb4b264e2de1ff
k3vur <kevin.urban@icloud.com>
2026-08-25 12:22:09 +0200

    font config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -8,4 +8,37 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Newsreader",
+      cssVariable: "--font-newsreader",
+      fallbacks: ["Iowan Old Style", "Palatino Linotype", "Georgia", "serif"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+      fallbacks: [
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "Segoe UI",
+        "sans-serif",
+      ],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Fira Code",
+      cssVariable: "--font-fira",
+      fallbacks: [
+        "ui-monospace",
+        "SF mono",
+        "SFMono-Regular",
+        "Menlo",
+        "Consolas",
+        "monospace",
+      ],
+    },
+  ],
 });
