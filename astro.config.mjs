e641f0d2dc941f985619a10002ad26af2d9a5752
k3vur <kevin.urban@icloud.com>
2026-08-26 10:39:39 +0200

    header

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,12 +13,15 @@ export default defineConfig({
       provider: fontProviders.google(),
       name: "Newsreader",
       cssVariable: "--font-newsreader",
+      styles: ["normal", "italic"],
+      weights: ["400 600"], // gets translated to 400..600 and includes opsz
       fallbacks: ["Iowan Old Style", "Palatino Linotype", "Georgia", "serif"],
     },
     {
       provider: fontProviders.google(),
       name: "Inter",
       cssVariable: "--font-inter",
+      weights: ["400 600"], // translated to 400..600
       fallbacks: [
         "ui-sans-serif",
         "system-ui",
@@ -31,6 +34,7 @@ export default defineConfig({
       provider: fontProviders.google(),
       name: "Fira Code",
       cssVariable: "--font-fira",
+      weights: [400, 500],
       fallbacks: [
         "ui-monospace",
         "SF mono",
