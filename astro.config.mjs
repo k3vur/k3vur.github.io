aa9b3fa747b22c3860241ae36e1bb0eb0509944d
k3vur <kevin.urban@icloud.com>
2026-09-18 11:50:10 +0200

    deploy github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://k3vur.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
